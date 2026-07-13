10f74d8820c1a249b9af498a58d6592d473421e1
devbylin <saulhstore@gmail.com>
2026-07-13 15:13:05 -0500

    recuperar componente/ocultar secciones/mostrar seccion/

---
+++ simulador_completo.js
@@ -0,0 +1,23 @@
+
+  let clientes = [];
+  let creditos = [];
+ 
+  let tasaInteres = 15;
+  let clienteSeleccionado = null;
+  let cuotaCalculada = 0;
+  let montoCalculado = 0;
+  let plazoCalculado = 0;
+  let creditoAprobado = false;
+
+  
+//Para recuperar o mostrar información usar los métodos de la clase utilitarios, 
+// puede agregar métodos adicionales en utilitarios
+
+function ocultarSecciones(){
+  document.getElementById("parametros").classList.remove("activa");
+  document.getElementById("clientes").classList.remove("activa");
+}
+function mostrarSeccion(parametros){
+  ocultarSecciones();
+ document.getElementById("parametros").classList.add("activa");
+}
