a19ffb11f4d0be765fe47e8ea8317c756b2f76d8
devbylin <saulhstore@gmail.com>
2026-07-13 17:05:06 -0500

    atributos/f.guardarTasa/mostrar mensaje

--- simulador_completo.js
+++ simulador_completo.js
@@ -21,3 +21,14 @@ function mostrarSeccion(parametros){
   ocultarSecciones();
  document.getElementById("parametros").classList.add("activa");
 }
+
+function guardarTasa(){
+  let valor = recuperarFloat("tasaInteres");
+
+  if(valor < 10 || valor > 20){
+    mostrarTexto('mensajeTasa','La tasa debe estar entre 10% y 20%');
+    return;
+  }
+  tasaInteres = valor;
+  mostrarTexto("mensajeTasa", "Tasa de interés guardada: " + tasaInteres + "%");
+}
