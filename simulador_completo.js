0334c09c09d6b59f55ba38532e1d24eec1246d29
devbylin <saulhstore@gmail.com>
2026-07-13 21:12:41 -0500

    guardar cliente

--- simulador_completo.js
+++ simulador_completo.js
@@ -9,7 +9,7 @@
   let plazoCalculado = 0;
   let creditoAprobado = false;
 
-  
+
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, 
 // puede agregar métodos adicionales en utilitarios
 
@@ -17,9 +17,13 @@ function ocultarSecciones(){
   document.getElementById("parametros").classList.remove("activa");
   document.getElementById("clientes").classList.remove("activa");
 }
-function mostrarSeccion(parametros){
+function mostrarSeccionParametros(parametros){
   ocultarSecciones();
  document.getElementById("parametros").classList.add("activa");
+}
+function mostrarSeccionClientes(clientes){
+  ocultarSecciones();
+  document.getElementById("clientes").classList.add("activa");
 }
 
 function guardarTasa(){
@@ -32,3 +36,38 @@ function guardarTasa(){
   tasaInteres = valor;
   mostrarTexto("mensajeTasa", "Tasa de interés guardada: " + tasaInteres + "%");
 }
+function guardarCliente(){
+  let cedula = recuperaraTexto("txtCedula");
+  let nombre = recuperaraTexto("txtNombre");
+  let apellido = recuperaraTexto("txtApellido");
+  let ingresos = recuperarInt("txtIngresos");
+  let egresos = recuperarInt("txtEgresos");
+  if(cedula == "" || nombre == "" || apellido == ""){
+    alert("Cedula, nombre y apellido son obligatorios");
+    return;
+  }
+  if(isNaN (ingresos) || ingresos < 0){
+    alert("Ingresa una valor valido para ingresos");
+    return;
+  }
+  if(isNaN (egresos) || egresos < 0){
+    alert("Ingresa una valor valido para egresos");
+    return;
+  }
+   let existe = clientes.some(clientes => clientes.cedula === cedula);
+    if (existe) {
+        alert("Ya existe un cliente con la cédula: " + cedula);
+        return;
+    }
+  let nuevoCliente = {
+    cedula: cedula,
+    nombre: nombre,
+    apellido: apellido,
+    ingresos: ingresos,
+    egresos: egresos,
+  };
+  alert("Cliente agregado correctamente");
+    limpiarFormulario();
+    pintarTablaClientes();  
+}
+
